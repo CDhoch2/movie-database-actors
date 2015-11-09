71d30386586e42f475be3d41b3b3f574a2eb2723
mro- <marlen.rotert@gmail.com>
2015-11-09 10:45:47 +0100

    Update dockerhub-push.sh

--- Jenkins/dockerhub-push.sh
+++ Jenkins/dockerhub-push.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 # DockerHub Login
-sudo /home/ec2-user/docker-latest login -e ${dockerhub_mail} -u ${dockerhub_user} -p ${dockerhub_pwd}
+/home/ec2-user/docker-latest login -e ${dockerhub_mail} -u ${dockerhub_user} -p ${dockerhub_pwd}
 
 #Tag images
-sudo /home/ec2-user/docker-latest tag -f moviedatabase_actors cdzwei/mvdb_actors:latest
+/home/ec2-user/docker-latest tag -f moviedatabase_actors cdzwei/mvdb_actors:latest
 
 # Push to DockerHub
-sudo /home/ec2-user/docker-latest push docker.io/cdzwei/mvdb_actors
+/home/ec2-user/docker-latest push docker.io/cdzwei/mvdb_actors
